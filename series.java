ca0d308f87371a5c3eceef27858fc4cca4c65955
Shivam Sharma <46440367+shivtech11@users.noreply.github.com>
2019-10-11 02:32:34 +0530

    first series

---
+++ series.java
@@ -0,0 +1,14 @@
+package firststep;
+
+public class series {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int n=1;
+    	for(int i=1;i<10;i++)
+    	{
+        n=n*2;
+        System.out.print(n+",");
+    	}
+
+		}
+}
